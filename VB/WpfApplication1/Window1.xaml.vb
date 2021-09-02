77f17727052d1c04730c49595ee5f2b3ddab4cb2
DevExpressExampleBot <fakeEmail>
2021-09-02 18:48:25 +0300

    Source auto update [skip ci]

--- VB/WpfApplication1/Window1.xaml.vb
+++ VB/WpfApplication1/Window1.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
@@ -20,6 +19,7 @@ Namespace WpfApplication1
 	''' </summary>
 	Partial Public Class Window1
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 
